eaf94bd0b99aaaaeb4435f040de73288623dbd5e
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-02-28 16:21:59 +0700

    pertemuan 3 & 4

---
+++ 03_GUI_Builder_dan_GitHub/HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(315, 163);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(168, 77);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click This";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
+
---
+++ 04_Automata_dan_Table-Driven_Construction/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+public class Program
+{
+    enum State { START, GAME, PAUSE, EXIT };
+    public static void Main()
+    {
+        State state = State.START;
+        string[] screenName = { "START", "GAME", "PAUSE", "EXIT" };
+        while (state != State.EXIT)
+        {
+            Console.WriteLine(screenName[(int)state] + " SCREEN");
+            Console.Write("Enter Command : ");
+            string command = Console.ReadLine();
+            switch (state)
+            {
+                case State.START:
+                    if (command == "ENTER")
+                        state = State.GAME;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.START;
+                    break;
+                case State.GAME:
+                    if (command == "ESC")
+                        state = State.PAUSE;
+                    else
+                        state = State.GAME;
+                    break;
+                case State.PAUSE:
+                    if (command == "BACK")
+                        state = State.GAME;
+                    else if (command == "HOME")
+                        state = State.START;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.PAUSE;
+                    break;
+            }
+        }
+        Console.WriteLine("EXIT SCREEN");
+    }
+}
